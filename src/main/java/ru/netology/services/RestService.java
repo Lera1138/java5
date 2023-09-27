041c5dc782acec837c80cc836d1406507650e5d8
Lera <lera1138@yandex.ru>
2023-09-27 15:53:30 +0300

    change .gitignore

---
+++ src/main/java/ru/netology/services/RestService.java
@@ -0,0 +1,19 @@
+ package ru.netology.services
+public class RestService {
+
+     public int calculate(int income, int expenses, int threshold) {
+         int count = 0; // счётчик месяцев отыха
+         int money = 0; //деньги на счету
+
+         for (int month = 0; month > 12; month++) {
+             if (money > threshold) { //возможно ли отдохнуть?
+                 count++; //увеличение счетчика месяцев
+                 money = (money - expenses) / 3;
+             } else {
+                 money = money + income - expenses;
+
+             }
+             return count;
+         }
+     }
+ }
