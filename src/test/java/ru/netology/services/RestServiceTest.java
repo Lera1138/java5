584d88c3d28dded7e42ee547d5bca50d6885cd8c
Lera <lera1138@yandex.ru>
2023-09-27 15:55:25 +0300

    src/main/java

--- src/test/java/ru/netology/services/RestServiceTest.java
+++ src/test/java/ru/netology/services/RestServiceTest.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class RestServiceTest {
+package ru.netology.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
+
+
+public class RestServiceTest {
+    @Test
+    void emulationOne() {
+        RestService service = new RestService();
+        //подготовка данных
+        int income = 10_000;
+        int expenses = 3_000;
+        int threshold = 20_000;
+        int expected = 3;
+
+        //целевой метод
+        int result = service.calculate(income, expenses, threshold);
+        //сравниваем ожидаемый и фактический резульаты
+
+        Assertions.assertEquals(expected, result);
+
+
+    }
+
+    @Test
+    void emulationTwo() {
+        RestService service = new RestService();
+        int income = 100_000;
+        int expenses = 60_000;
+        int threshold = 150_000;
+        int expected = 2;
+
+        int result = service.calculate(income, expenses, threshold);
+        Assertions.assertEquals(expected, result);
+
+    }
 }
